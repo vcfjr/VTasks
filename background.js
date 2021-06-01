58d9f34a67f24b29e40cac1e282c638df0dd1896
Himanshu Jangid <iamahalofan@gmail.com>
2021-06-01 12:00:26 +0530

    script update

--- background.js
+++ background.js
@@ -4,6 +4,15 @@ var form = document.getElementById("form");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     var item = document.createElement("li");
-    item.innerText = "document.getElementsByClassName()[0].value";
-    taskList.appendChild(item);
+    if (document.getElementsByClassName("at")[0].value != "") {
+        var data = "<button onclick='remove(event)'>-</button> <p> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;"+document.getElementsByClassName("at")[0].value+"</p>"
+        item.innerHTML = data ;
+        taskList.appendChild(item);
+    }
+
+    document.getElementsByClassName("at")[0].value = "";
 })
+
+function remove(e){
+    
+}
